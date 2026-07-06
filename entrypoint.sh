e203a0ff50932e0f31a643fbed9618f9c94309e4
lasto4kaqwert <lasto4kaqwert@gmail.com>
2026-07-06 16:38:41 +0300

    sh run file repair

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 
+alembic upgrade head
+
+python -u -m app.workers.events_sync_process &
+python -u -m app.workers.ticket_outbox_process &
+python -u -m app.workers.capashino_outbox_process &
+
 exec "$@"
