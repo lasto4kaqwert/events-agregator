50380ff01e6de5f03a67a04b1fc8793703377294
lasto4kaqwert <lasto4kaqwert@gmail.com>
2026-07-07 11:52:42 +0300

    glitchtip tracking with lint check

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,9 +1,8 @@
+from .debug import router as debug_router
 from .events import router as events_router
 from .health import router as health_router
 from .sync import router as sync_router
 from .tickets import router as tickets_router
-from .debug import router as debug_router
-
 
 routers = [
     events_router,
